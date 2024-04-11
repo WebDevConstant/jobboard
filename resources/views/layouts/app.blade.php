1b5e1bbccbd2167d6e547610f50b0eb811cf25ae
TannyEXY <tanakasteven450@gmail.com>
2024-04-11 12:47:02 +0200

    chane

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,10 +48,10 @@
               <li class="d-lg-none"><a href="post-job.html"><span class="mr-2">+</span> Post a Job</a></li>
               @guest
                             @if (Route::has('login'))
-                                <li class="d-lg-none"><a href="{{ route('login') }}">Log In</a></li>
+                                <li class=""><a href="{{ route('login') }}">Log In</a></li>
                             @endif
                             @if (Route::has('register'))
-                                <li class="d-lg-none"><a href="{{ route('register') }}">Register</a></li>
+                                <li class=""><a href="{{ route('register') }}">Register</a></li>
                             @endif
               @else
                             <li class="nav-item dropdown">
